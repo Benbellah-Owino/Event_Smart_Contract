f2f3f5468a20a84c4778a586c68d8daf456ab981
Benbellah-Owino <owinobenbellah@gmail.com>
2023-10-12 01:09:46 +0300

    First Commit

---
+++ frontend/src/lib/events_components/event_tile.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<main
+	class="event_tile w-64 h-48 md:w-80 md:h-64 mb-8 border border-black rounded-lg cursor-pointer duration-300 hover:border-purple-400"
+	id="event_tile "
+>
+	<div class="logo h-16 md:h-32 rounded-t-lg bg-blue-200" id="log">H</div>
+	<h1 class="event_title ml-2 mb-3 font-bold text-lg" id="event_title">
+		KCA University hackethorn
+	</h1>
+	<div class="more_info w-full flex justify-between row text-sm" id="more_info">
+		<h3 class="venue w-2/3 ml-2" id="venue"><b>Venue: </b>KCA university</h3>
+		<h3 class="price w-1/3" id="id"><b>price: </b>7843</h3>
+	</div>
+	<h3 class="date_time ml-2 mt-2 text-sm" id="data_time">Wed 1st Nov 2021 10:00am</h3>
+	<button
+		class="learn_more w-30 p-1 float-right mr-2 mt-1 border border-black rounded-lg bg-yellow-300 text-xs md:text-sm md:font-bold duration-300 hover:bg-black hover:text-yellow-300 hover:shadow-lg hover:shadow-yellow-300"
+		id="learn_more">LEARN MORE</button
+	>
+</main>
---
+++ frontend/src/routes/events/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Navbar from '$lib/major_components/navbar.svelte';
+	import EventTile from '$lib/events_components/event_tile.svelte';
+</script>
+
+<div class="main_events flex justify-start" id="main_events">
+	<Navbar />
+	<div class="events w-full h-full ml-16 md:ml-36 pt-2 pl-10 lg:grid lg:grid-cols-3">
+		<EventTile />
+		<EventTile />
+		<EventTile />
+		<EventTile />
+		<EventTile />
+		<EventTile />
+		<EventTile />
+	</div>
+</div>
