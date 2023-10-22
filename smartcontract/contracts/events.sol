7bf708335d68070fbd3c114070dda13fbc55d36e
Benbellah-Owino <owinobenbellah@gmail.com>
2023-10-22 21:08:28 +0300

    ADD:-> Payment to smart contract functionality

--- smartcontract/contracts/events.sol
+++ smartcontract/contracts/events.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.18;
 
 contract EventsGroup10{
 
@@ -26,6 +26,13 @@ contract EventsGroup10{
         string event_id;
         address owner;
         uint timeissued;
+        TicketStatus status;
+    }
+
+    enum TicketStatus{
+        VALID,
+        USED,
+        INVALIDATED
     }
 
     //Array definitions
@@ -38,32 +45,34 @@ contract EventsGroup10{
 
     mapping(address => Event[]) private events;
 
+    mapping(address => Ticket[]) tickets;
+
 
    
 //DappCreators functions
-    constructor() private{
+    constructor () payable {
          DappCreators.push(msg.sender);
     }
 
-    function addCreators(address _owner){
+    function addCreators(address _owner) private{
         DappCreators.push(_owner);
     }
  
-    function listCreators() public view returns address[]{
+    function listCreators() public view returns (address[] memory){
         return DappCreators;
     }
 
     //Event functions
-    function createEvent(string memory _db_id, uint _no_Of_tickets, uint256 _price)public view returns (Event memory){
+    function createEvent(string memory _db_id, uint _no_Of_tickets, uint256 _price)public returns (Event memory){
         Event memory new_event;
 
         new_event.id = events[msg.sender].length;
-        new_event._db_id = _db_id;
+        new_event.db_id = _db_id;
         new_event.noOfTickets = _no_Of_tickets;
         new_event.price = _price;
         new_event.organizer = msg.sender;
         new_event.completed = false;
-        new_event.timecreated = block.timestamp
+        new_event.timecreated = block.timestamp;
 
         events[msg.sender].push(new_event);
       
@@ -71,17 +80,17 @@ contract EventsGroup10{
         return new_event;
     }
 
-    function listOrganizersEvents(address _organizers)public returns (Event[] memory) {
+    function listOrganizersEvents(address _organizers)public view returns (Event[] memory) {
         return events[_organizers];
     }
 
-    function getSpecificEvent(address _organizer, uint _id) public returns (Event memory){
+    function getSpecificEvent(address _organizer, uint _id) public view returns (Event memory){
         return events[_organizer][_id];
     }
 
     //Organizer functions
     function create_organizer(string memory _name, string memory _id) public{
-        Organizer newOrganizer;
+        Organizer memory newOrganizer;
         newOrganizer.name = _name;
         newOrganizer.timejoined = block.timestamp;
         newOrganizer.organizer_id = _id;
@@ -94,12 +103,27 @@ contract EventsGroup10{
 
 
     //Ticket Function
-    modifier correctBalance(uint price){
-        require(sender.balance > price, "You do not have enough ETH to buy this tickets");
-        _;
-    }
-    function buyTicket(uint256 _id ){
+    // modifier correctBalance(uint price) {
+    //     require( > price, "You do not have enough ETH to buy this tickets");
+    //     _;
+    // }
+    function buyTicket(string memory _ticketId, string memory _eventId,uint _price, address _to ) public  payable returns (Ticket memory){
+        require(msg.value >= _price, "Wrong amount");
+
+        Ticket memory new_ticket;
+        new_ticket.id = tickets[msg.sender].length;
+        new_ticket.ticket_id = _ticketId;
+        new_ticket.event_id = _eventId;
+        new_ticket.owner = msg.sender;
+        new_ticket.timeissued = block.timestamp;
+        new_ticket.status = TicketStatus.VALID;
+
+        tickets[msg.sender].push(new_ticket);
+
         
+
+        return new_ticket;
+
     }
 
 }
